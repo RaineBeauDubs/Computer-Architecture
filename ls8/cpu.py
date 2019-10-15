f5b2287b0aa558b8d955265dd51572285ecb146f
Raine Wallace <rainebeaudubs@gmail.com>
2019-10-15 17:08:11 -0400

    MUL is working, omg

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,8 +7,11 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
+        self.ram = [0] * 256
+        self.reg = [0] * 8
+        self.pc = 0
 
-    def ram_read(self, MAR)
+    def ram_read(self, MAR):
         # should find and return value at given address in memory
         # _Memory Address Register_ (MAR)
         return self.ram[MAR]
@@ -22,22 +25,28 @@ class CPU:
         """Load a program into memory."""
 
         address = 0
+        if len(sys.argv) < 2:
+            print("A program name is required.")
+            sys.exit()
 
         # For now, we've just hardcoded a program:
 
-        program = [
-            # From print8.ls8
-            0b10000010, # LDI R0,8
-            0b00000000,
-            0b00001000,
-            0b01000111, # PRN R0
-            0b00000000,
-            0b00000001, # HLT
-        ]
+        # program = [
+        #     # From print8.ls8
+        #     0b10000010, # LDI R0,8
+        #     0b00000000,
+        #     0b00001000,
+        #     0b01000111, # PRN R0
+        #     0b00000000,
+        #     0b00000001, # HLT
+        # ]
 
-        for instruction in program:
-            self.ram[address] = instruction
-            address += 1
+        with open(sys.argv[1]) as f:
+            for line in f:
+                if line[0] != '#' and line !='\n':
+                    self.ram[address] = int(line[0:8], 2)
+                    address += 1
+            f.closed
 
 
     def alu(self, op, reg_a, reg_b):
@@ -45,6 +54,8 @@ class CPU:
 
         if op == "ADD":
             self.reg[reg_a] += self.reg[reg_b]
+        elif op == "MUL":
+            self.reg[reg_a] *= self.reg[reg_b]
         #elif op == "SUB": etc
         else:
             raise Exception("Unsupported ALU operation")
@@ -71,4 +82,30 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+        HLT = 0b00000001
+        LDI = 0b10000010
+        PRN  = 0b01000111 
+        PUSH = 0b01000101 
+        POP  = 0b01000110 
+        MUL  = 0b10100010
+        running = True
+        while running:
+            # the _Instruction Register_, local variable
+            IR = self.ram[self.pc]
+            # reads the next two pieces of data
+            operand_a = self.ram_read(self.pc + 1)
+            operand_b = self.ram_read(self.pc + 2)
+            if IR == HLT:
+                running = False
+            elif IR == LDI:
+                self.reg[operand_a] = operand_b
+                self.pc += 3
+            elif IR == PRN:
+                print(self.reg[operand_a])
+                self.pc += 2
+            elif IR == MUL:
+                self.alu("MUL", operand_a, operand_b)
+                self.pc += 3
+            else:
+                print("Halting the program")
+                running = False
